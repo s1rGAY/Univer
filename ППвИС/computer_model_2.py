046c6ea9ed273253a0bc4aeb562cc60fcad533fd
s1rGAY <rassafonovs@gmail.com>
2022-04-04 18:15:30 +0300

    Rebuild from univer. Checked main architecture.

--- ППвИС/computer_model_2.py
+++ ППвИС/computer_model_2.py
@@ -3,6 +3,7 @@ from multiprocessing.reduction import sendfds
 import os
 
 #probably Done
+#mb пофиксить вывод данных для удобного парсинга
 class User:
     def __init__(self, login, password, access_lvl, system_type):
         self.login = login
@@ -33,6 +34,7 @@ class User:
         else:
             print('Access denied!')
 
+#добавить от какого пользователя запустился компьютер
 class Computer:
     def __init__(self, storage_size):
         #тк комп создается впервые нужно задать хотя бы 1 пользователя
@@ -42,17 +44,63 @@ class Computer:
         self.users = [User(input(),input(),input(),input())]
         self.storage = Storage(storage_size, self.computer_name, self.users)
         self.keyboard = Keyboard()
+        self.comp_status = True
+        self.logged_user = self.users[0]
+
+    def turn_computer_status(self):
+        if self.comp_status == True:
+            self.comp_status = False
+        else:
+            self.comp_status = True
+
+    #Всё это по идее должна делать операционная система, но тогда необходимо переделалть хиранилище
+    #добавть логин при включении
+    def login(self):
+        if self.check_access_rights()[0] != False:
+            self.logged_user = self.check_access_rights()[1]
+                 
+    def check_access_rights(self):
+        for j in range(3):
+            print('Enter Login :')
+            login = input()
+            for i in self.users:
+                if i.login == login:
+                    for a in range(3):
+                        print('Enter password :')
+                        password = input()
+                        if i.password == password:
+                            return (True, i)
+                        else:
+                            print('Invalid password! Attemps ramained : '+str(a))
+                elif i == self.users[-1]:
+                    print('Access denied!')
+                    return (False, None)
+                print('Invalid login! Attemps ramained :'+str(j))
+        
+    def change_user(self):
+        self.login()  
 
     def add_user(self):
         #тк комп с юзером уже есть, надо просто добавить нового через self.storage
+        #добавить проверку на существование такого логина
         print('Enter info in order:\n Login \n Password \n Access_lvl \n System_type')
-        self.users.append(User[input(),input(),input(),input()])
+        self.users.append(User(input(),input(),input(),input()))
         self.storage.add_new_user_direcrory(self.users[-1])
     
     def del_user(self):
-        print('Enter user login : ')
-        login = input()
+        print('To remove user you must know it\'s login and password.')
+        permission_data = self.check_access_rights()
+        if permission_data[0] == True:
+            self.storage.delete_user_directory(permission_data[1])
+            self.users.remove(permission_data[1])
+        
         #проверка на существование
+
+    def enter_command(self):
+        command = self.keyboard.typing()
+        if self.logged_user.operation_system.commands.get_command(command)==True:
+            #выполнение команды
+            pass
 
 
 #добавить проверку на существования уже такого компа
@@ -109,6 +157,7 @@ class Storage:
                 self.system_storage_size : ' free system memory.',
                 self.user_storage_size :' free user memory'}
     
+    #добавть проверку на существование такого логина
     def add_new_user_direcrory(self,login):
         os.chdir((self.path_to_comp+'/System_storage'))
         os.mkdir(login.login)
@@ -153,49 +202,86 @@ class Сommand_settings:
     def del_command(self, command):  # just a command
         del self.commands[command]
 
+class Commands_realization:
+    def clear_file(file_name):
+        os.system(r' >file_name')  # ???????????????????????????
+
+    def delete_file(file_name):
+        os.remove(str(file_name))
+
+    def create_file(file_name):
+        file = open(str(file_name), "w")
+        file.close()
+
+    def overwrite_file(file_name):
+        file = open(str(file_name), "w")
+        file.write(str(input()))
+        file.close()
+
+    def add_data_to_end(file_name):
+        file = open(str(file_name), "a")
+        file.write(str(input()))
+        file.close()
+
+    def view_file_data(file_name):
+        file = open(str(file_name), "r")
+        file.read()
+        file.close()
+
+
 class Linux_commands(Сommand_settings):
     def __init__(self):
         self.commands = {'shoutdown': 'off',
                           'clear_file': 'clearing file',
                           'rm': 'remove file',
                           'touch': 'create file',
-                          'echo >': 'overwrite file',
-                          'echo >>': 'add data to end',
+                          'echo_>': 'overwrite file',
+                          'echo_>>': 'add data to end',
                           'cat': 'view file data'}
 
 class Win_commands(Сommand_settings):
     def __init__(self):
         self.commands = {'shoutdown': 'off',
-                        'cat /dev/null': 'clearing file',
+                        'cat_/dev/null': 'clearing file',
                         'del': 'remove file',
-                        'type null >': 'create file',
-                        'echo >': 'overwrite file',
-                        'echo >>': 'add data to end',
+                        'type_null_>': 'create file',
+                        'echo_>': 'overwrite file',
+                        'echo_>>': 'add data to end',
                         'cat': 'view file data'}
 
 class macOS_commands(Сommand_settings):
     def __init__(self):
         self.commands = {'shoutdown': 'off',
-                          'echo -n': 'clearing file',
+                          'echo_-n': 'clearing file',
                           'rm': 'remove file',
                           'nano': 'create file',
                           'echo >': 'overwrite file',
                           'echo >>': 'add data to end',
                           'cat': 'view file data'}
 
-#добавить управление и взаимодействие юзера
+#будет хранить реализацию команд
 class Operation_system:
-    def __init__(self, system_name):
-        self.type = system_name
-
-        if system_name.lower() == 'linux':
+    def __init__(self, system_type):
+        self.type = system_type
+        self.commands = None
+        self.commands_sourse = Commands_realization()
+        if system_type.lower() == 'linux':
+            self.type = system_type
             self.commands = Linux_commands()
-        elif system_name.lower() == 'windows':
+        elif system_type.lower() == 'windows':
+            self.type = system_type
             self.commands = Win_commands()
-        elif system_name == 'macosx':
-            self.command == macOS_commands()
+        elif system_type.lower() == 'macosx':
+            self.type = system_type
+            self.commands = macOS_commands()
         else:
             print('Invalid operating system name!')
     
     def get_commands(self):
         return self.commands.get_commands()
+
+    def command_processing(self, command):
+        command = str(command).replace(' ','_')
+
+a = (True, 123)
+print(a[0])
