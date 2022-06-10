2ca4e88e6f1c57a2c54c3b3acd7d87541fbace83
s1rGAY <rassafonovs@gmail.com>
2022-06-10 17:23:35 +0300

    restructuring of the repository by terms

--- ППвИС/lab-4/abstractions/observer.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractclassmethod, abstractmethod
-
-
-class Observer(ABC):
-    @abstractmethod
-    def handle(self) -> None:
-        """Does changes after event is fired."""
